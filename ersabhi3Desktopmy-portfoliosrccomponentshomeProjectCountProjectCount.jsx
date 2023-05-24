05bfde7df13c4768569e090d06db071b989637fe
abhishek pandey <kakupandey2017@gmail.com>
2023-05-24 18:57:52 +0530

    new update

---
+++ ersabhi3Desktopmy-portfoliosrccomponentshomeProjectCountProjectCount.jsx
@@ -0,0 +1,29 @@
+[33mcommit 7b1abca8b89906cee006a51e1907d1aebdfd2952[m[33m ([m[1;36mHEAD -> [m[1;32mmain[m[33m)[m
+Author: abhishek pandey <kakupandey2017@gmail.com>
+Date:   Wed May 24 18:54:36 2023 +0530
+
+    new update
+
+[33mcommit 984168124babed2898c28d04df2b0bd9771832d2[m[33m ([m[1;31morigin/main[m[33m)[m
+Author: abhishek pandey <kakupandey2017@gmail.com>
+Date:   Wed May 24 12:07:03 2023 +0530
+
+    new update
+
+[33mcommit 92a656a6cb96285b41b14744eeb933bfc5ccf94a[m
+Author: abhishek pandey <kakupandey2017@gmail.com>
+Date:   Mon May 8 23:50:53 2023 +0530
+
+    adding hero section
+
+[33mcommit 356ed15d06f83332bad111cc1c7fb5602a027be2[m
+Author: abhishek pandey <kakupandey2017@gmail.com>
+Date:   Sun May 7 19:43:32 2023 +0530
+
+    intial commit
+
+[33mcommit 9d1c1e487135f9d3df10e531169fd9768e5dbe25[m
+Author: abhishek pandey <kakupandey2017@gmail.com>
+Date:   Fri May 5 15:50:14 2023 +0530
+
+    Initial commit from Create Next App
